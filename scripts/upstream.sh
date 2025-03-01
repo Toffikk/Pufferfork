add4f2bd81d898545a781cd25e203db604fa5838
Toffikk <slodkitofik@gmail.com>
2025-03-02 00:28:25 +0100

    [ci skip] update upstream script

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -6,7 +6,7 @@ exit_on_error() {
 git reset HEAD --hard
 
 oldHash=$(grep "paperRef=" gradle.properties | cut -d "=" -f2)
-newHash=$(curl -s https://api.github.com/repos/PaperMC/paper/commits/master | jq -r .sha)
+newHash=$(curl -s https://api.github.com/repos/PaperMC/paper/commits/main | jq -r .sha)
 
 if [ "$oldHash" = "$newHash" ]; then
     echo "Upstream has not updated!"
@@ -18,8 +18,9 @@ echo "Updating paper: $oldHash -> $newHash"
 sed -i "s/$oldHash/$newHash/g" gradle.properties
 git add gradle.properties
 
-./gradlew applyPatches || exit_on_error "An error occurred when merging patches!"
-./gradlew rebuildPatches || exit_on_error "An error occurred when rebuilding patches!"
+./gradlew applyAllPatches || exit_on_error "An error occurred when merging patches!"
+./gradlew rebuildAllServerPatches || exit_on_error "An error occurred when rebuilding server patches!"
+./gradlew rebuildPaperApiPatches || exit_on_error "An error occurred when rebuilding api patches!"
 ./gradlew createMojmapPaperclipJar || exit_on_error "An error occurred when building!"
 
 scripts/upstreamCommit.sh $oldHash $newHash
