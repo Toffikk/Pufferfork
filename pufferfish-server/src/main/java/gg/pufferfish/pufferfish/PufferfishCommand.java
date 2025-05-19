d46583e9bb3d9462d444d4afb1ec4fee18c1fca9
Toffikk <slodkitofik@gmail.com>
2025-05-19 15:02:12 +0200

    Should work good -> cleaned up client output

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
@@ -7,6 +7,7 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import com.mojang.brigadier.Command;
 import io.papermc.paper.command.brigadier.Commands;
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import java.util.Set;
 
 public class PufferfishCommand {
@@ -29,10 +30,24 @@ public class PufferfishCommand {
             .build();
     }
 
+    public static LiteralCommandNode<CommandSourceStack> createHidden() {
+        final PufferfishVersionCommand command_ver = new PufferfishVersionCommand();
+        final PufferfishReloadCommand command_rel = new PufferfishReloadCommand();
+
+        return Commands.literal("pufferfish")
+            .requires(source -> false)
+            .then(Commands.literal("version")
+                .executes(command_ver::versionCommand)
+            )
+            .then(Commands.literal("reload")
+                .executes(command_rel::reloadCommand)
+            )
+            .build();
+    }
     public static void registerCommands() {
         // dirty hack
-        registerInternalCommand(create(), "pufferfish", PufferfishVersionCommand.DESCRIPTION, List.of("pufferfish version"), Set.of());
-        registerInternalCommand(create(), "pufferfish", PufferfishReloadCommand.DESCRIPTION, List.of("pufferfish reload"), Set.of());
+        registerInternalCommand(createHidden(), "pufferfish", PufferfishVersionCommand.DESCRIPTION, List.of("pufferfish version"), Set.of());
+        registerInternalCommand(createHidden(), "pufferfish", PufferfishReloadCommand.DESCRIPTION, List.of("pufferfish reload"), Set.of());
         registerInternalCommand(create(), "pufferfish", DESCRIPTION, List.of("pufferfish"), Set.of()); // has to always be declared last otherwise other descriptions bleed over
     }
 
