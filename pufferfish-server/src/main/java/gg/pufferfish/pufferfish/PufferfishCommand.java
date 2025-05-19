397e2a81071e7ed8db0baec03d43dd2273eb38e0
Toffikk <slodkitofik@gmail.com>
2025-05-19 13:21:43 +0200

    Rework the pufferfish command using brigadier

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
@@ -1,68 +1,50 @@
 package gg.pufferfish.pufferfish;
 
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.md_5.bungee.api.ChatColor;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandRegistrationFlag;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import com.mojang.brigadier.Command;
+import io.papermc.paper.command.brigadier.Commands;
 import net.minecraft.server.MinecraftServer;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import java.util.Set;
 
-public class PufferfishCommand extends Command {
+public class PufferfishCommand {
+    public static final String DESCRIPTION = "The pufferfish command";
 
-    public PufferfishCommand() {
-        super("pufferfish");
-        this.description = "Pufferfish related commands";
-        this.usageMessage = "/pufferfish [reload | version]";
-        this.setPermission("bukkit.command.pufferfish");
-    }
-    
-    public static void init() {
-        MinecraftServer.getServer().server.getCommandMap().register("pufferfish", "Pufferfish", new PufferfishCommand());
-    }
+    public static LiteralCommandNode<CommandSourceStack> create() {
+        final PufferfishVersionCommand command_ver = new PufferfishVersionCommand();
+        final PufferfishReloadCommand command_rel = new PufferfishReloadCommand();
 
-    @Override
-    public List<String> tabComplete(CommandSender sender, String alias, String[] args, Location location) throws IllegalArgumentException {
-        if (args.length == 1) {
-            return Stream.of("reload", "version")
-              .filter(arg -> arg.startsWith(args[0].toLowerCase()))
-              .collect(Collectors.toList());
-        }
-        return Collections.emptyList();
+        return Commands.literal("pufferfish")
+            .requires(source -> source.getSender().hasPermission("bukkit.command.pufferfish"))
+            .then(Commands.literal("version")
+                .requires(source -> source.getSender().hasPermission("bukkit.command.pufferfish.version"))
+                .executes(command_ver::versionCommand)
+            )
+            .then(Commands.literal("reload")
+                .requires(source -> source.getSender().hasPermission("bukkit.command.pufferfish.reload"))
+                .executes(command_rel::reloadCommand)
+            )
+            .build();
     }
 
-    @Override
-    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        if (!testPermission(sender)) return true;
-        String prefix = ChatColor.of("#12fff6") + "" + ChatColor.BOLD + "Pufferfish » " + ChatColor.of("#e8f9f9");
+    public static void registerCommands() {
+        // dirty hack
+        registerInternalCommand(create(), "pufferfish", PufferfishVersionCommand.DESCRIPTION, List.of("version"), Set.of());
+        registerInternalCommand(create(), "pufferfish", PufferfishReloadCommand.DESCRIPTION, List.of("reload"), Set.of());
+        registerInternalCommand(create(), "pufferfish", DESCRIPTION, List.of("pufferfish"), Set.of()); // has to always be declared last otherwise other descriptions bleed over
+    }
 
-        if (args.length != 1) {
-            sender.sendMessage(prefix + "Usage: " + usageMessage);
-            args = new String[]{"version"};
-        }
-
-        if (args[0].equalsIgnoreCase("reload")) {
-            MinecraftServer console = MinecraftServer.getServer();
-            try {
-                PufferfishConfig.load();
-            } catch (IOException e) {
-                sender.sendMessage(Component.text("Failed to reload.", NamedTextColor.RED));
-                e.printStackTrace();
-                return true;
-            }
-            console.server.reloadCount++;
-
-            Command.broadcastCommandMessage(sender, prefix + "Pufferfish configuration has been reloaded.");
-        } else if (args[0].equalsIgnoreCase("version")) {
-            Command.broadcastCommandMessage(sender, prefix + "This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")");
-        }
-
-        return true;
+    private static void registerInternalCommand(final LiteralCommandNode<CommandSourceStack> node, final String namespace, final String description, final List<String> aliases, final Set<CommandRegistrationFlag> flags) {
+        io.papermc.paper.command.brigadier.PaperCommands.INSTANCE.registerWithFlagsInternal(
+            null,
+            namespace,
+            "Pufferfish",
+            node,
+            description,
+            aliases,
+            flags
+        );
     }
 }
