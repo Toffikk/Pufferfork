5d91819c0397595b2b7b6942f02be28ee095a5bf
Toffikk <slodkitofik@gmail.com>
2025-06-17 15:00:22 +0200

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@889d78a Add registry builders for SoundEvent and JukeboxSong (#11805)
    PaperMC/Paper@6fddc93 keep original generics on now-deprecated freeze
    PaperMC/Paper@93c2a05 Dialog registry API
    PaperMC/Paper@dfc6f7b Initial solution for RegistryValueSet<Dialog> during bootstrap

---
+++ pufferfork-server/src/main/java/gg/pufferfish/pufferfish/PufferfishFlareCommand.java
@@ -0,0 +1,48 @@
+package gg.pufferfish.pufferfish;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextDecoration;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.Bukkit;
+
+public class PufferfishVersionCommand {
+    public static final String DESCRIPTION = "Returns the server version";
+
+    final TextComponent prefix = Component.text()
+            .color(TextColor.color(0x12fff6))
+            .decoration(TextDecoration.BOLD, true)
+            .append(Component.text(" Pufferfork » "))
+            .build();
+
+        public int versionCommand(final CommandContext<CommandSourceStack> context) {
+            sendVersionCommand(context.getSource().getSender());
+            return Command.SINGLE_SUCCESS;
+        }
+
+        private void sendVersionCommand(final CommandSender sender) {
+            final TextComponent commandSender = Component.text()
+                .append(Component.text(sender.getName() + ":"))
+                .decoration(TextDecoration.BOLD, true)
+                .build();
+
+            TextComponent versionMessage = Component.text("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")");
+            TextComponent versionComponent = Component.text()
+                .color(TextColor.color(0xe8f9f9))
+                .append(commandSender)
+                .append(prefix)
+                .append(versionMessage)
+                .hoverEvent(Component.translatable("chat.copy.click"))
+                .clickEvent(ClickEvent.copyToClipboard(versionMessage.content()))
+                .build();
+
+            sender.sendMessage(versionComponent);
+        }
+    }
