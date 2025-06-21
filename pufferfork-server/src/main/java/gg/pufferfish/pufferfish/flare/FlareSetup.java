6d58ab707536d7a84a732a7ea1fe1e0a5a484643
Toffikk <slodkitofik@gmail.com>
2025-06-21 16:30:25 +0200

    Handle flare initialization errors more gracefully

--- pufferfork-server/src/main/java/gg/pufferfish/pufferfish/flare/FlareSetup.java
+++ pufferfork-server/src/main/java/gg/pufferfish/pufferfish/flare/FlareSetup.java
@@ -2,32 +2,37 @@ package gg.pufferfish.pufferfish.flare;
 
 import co.technove.flare.FlareInitializer;
 import co.technove.flare.internal.profiling.InitializationException;
-import net.minecraft.server.MinecraftServer;
-import org.apache.logging.log4j.Level;
+import gg.pufferfish.pufferfish.PufferfishLogger;
+import java.util.Locale;
 
 public class FlareSetup {
 
     private static boolean initialized = false;
     private static boolean supported = false;
+    private static final String OS_NAME = System.getProperty("os.name").toLowerCase(Locale.ROOT);
 
     public static void init() {
         if (initialized) {
             return;
         }
 
+        if (!(OS_NAME.contains("linux") || OS_NAME.contains("mac"))) {
+            PufferfishLogger.LOGGER.warn("Flare does not support running on {}, will not enable!", OS_NAME);
+            return;
+        }
+
         initialized = true;
         try {
             for (String warning : FlareInitializer.initialize()) {
-                MinecraftServer.LOGGER.warn("Flare warning: {}", warning);
+                PufferfishLogger.LOGGER.warn("Flare warning: {}", warning);
             }
             supported = true;
         } catch (InitializationException e) {
-            MinecraftServer.LOGGER.warn("Failed to enable Flare:", e);
+            PufferfishLogger.LOGGER.warn("Failed to enable Flare:", e);
         }
     }
 
     public static boolean isSupported() {
         return supported;
     }
-
 }
