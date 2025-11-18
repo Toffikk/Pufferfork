0f2c0048cab31e5a714d826c02b3e49eb8d8d5a8
Toffikk <slodkitofik@gmail.com>
2025-11-18 20:30:42 +0100

    feat: cache the profiling uri

--- pufferfork-server/src/main/java/gg/pufferfish/pufferfish/flare/FlareCommand.java
+++ pufferfork-server/src/main/java/gg/pufferfish/pufferfish/flare/FlareCommand.java
@@ -39,6 +39,7 @@ public class FlareCommand {
         .append(Component.text("]", NamedTextColor.DARK_GRAY))
         .append(Component.text(" "))
         .build();
+    private static String PROFILING_URI = "";
 
     public static void init() {
 
@@ -93,11 +94,10 @@ public class FlareCommand {
                     CommandSender sender = ctx.getSource().getSender();
 
                     MCUtil.scheduleAsyncTask(() -> {
-                        String profile = ProfilingManager.getProfilingUri();
                         if (ProfilingManager.stop()) {
                             broadcastPrefixed(
                                     Component.text("Profiling has been stopped.", MAIN_COLOR),
-                                    Component.text(profile, HEX).clickEvent(ClickEvent.openUrl(profile))
+                                    Component.text(PROFILING_URI, HEX).clickEvent(ClickEvent.openUrl(PROFILING_URI))
                             );
                         }
                     });
@@ -159,13 +159,14 @@ public class FlareCommand {
         MCUtil.scheduleAsyncTask(() -> {
             try {
                 if (ProfilingManager.start(profileType)) {
+                    PROFILING_URI = ProfilingManager.getProfilingUri();
                     broadcastPrefixed(
                         Component.text("Flare has been started!", MAIN_COLOR),
                         Component.text("It will run in the background for 15 minutes", NamedTextColor.GRAY),
                         Component.text("or until manually stopped using:", NamedTextColor.GRAY),
                         Component.text("  ").append(Component.text("/flare stop", NamedTextColor.WHITE).clickEvent(ClickEvent.runCommand("flare stop"))),
                         Component.text("Follow its progress here:", NamedTextColor.GRAY),
-                        Component.text(ProfilingManager.getProfilingUri(), HEX).clickEvent(ClickEvent.openUrl(ProfilingManager.getProfilingUri()))
+                        Component.text(PROFILING_URI, HEX).clickEvent(ClickEvent.openUrl(PROFILING_URI))
                     );
                 }
             } catch (UserReportableException e) {
@@ -200,10 +201,9 @@ public class FlareCommand {
     }
 
     protected static void broadcastException() {
-        String profilingUri = ProfilingManager.getProfilingUri();
         broadcastPrefixed(
                 Component.text("An exception happened and profiling has stopped", MAIN_COLOR),
-                Component.text(profilingUri, HEX).clickEvent(ClickEvent.openUrl(profilingUri))
+                Component.text(PROFILING_URI, HEX).clickEvent(ClickEvent.openUrl(PROFILING_URI))
         );
     }
 
